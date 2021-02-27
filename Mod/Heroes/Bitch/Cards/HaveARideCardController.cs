370b02033a9cd6da9a63353cc3e6f85fc5639a5a
James Picone <jamesmpicone@gmail.com>
2021-02-27 22:50:02 +1100

    Add all of Bitch's cards to the decklist (must fix flavour text)

---
+++ Mod/Heroes/Bitch/Cards/HaveARideCardController.cs
@@ -0,0 +1,20 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jp.ParahumansOfTheWormverse.Bitch
+{
+    public class HaveARideCardController : CardController
+    {
+        public HaveARideCardController(Card card, TurnTakerController controller) : base(card, controller)
+        { }
+
+        public override System.Collections.IEnumerator Play()
+        {
+            yield break;
+        }
+    }
+}
