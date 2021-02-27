36d5f592f79e35884009a0a8e8454e4c8f387f32
James Picone <jamesmpicone@gmail.com>
2021-02-27 19:44:13 +1100

    Initially-loading mod

---
+++ Mod/Heroes/Bitch/Characters/BitchCharacterCardController.cs
@@ -0,0 +1,26 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jp.ParahumansOfTheWormverse.Bitch
+{
+    public class BitchCharacterCardController : HeroCharacterCardController
+    {
+        public BitchCharacterCardController(Card card, TurnTakerController controller) : base(card, controller)
+        { }
+
+        public override IEnumerator UseIncapacitatedAbility(int index)
+        {
+            yield break;
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            yield break;
+        }
+    }
+}
