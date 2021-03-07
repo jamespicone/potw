2d7f69b63a6117931bbbc8893ae8236b2c38934a
James Picone <jamesmpicone@gmail.com>
2021-03-07 18:30:52 +1100

    Some minor reshuffling of decklist folder structure. Start putting Lung stuff in.

---
+++ Mod/Villains/Lung/Cards/BruteCardController.cs
@@ -0,0 +1,16 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jp.ParahumansOfTheWormverse.Lung
+{
+    public class BruteCardController : CardController
+    {
+        public BruteCardController(Card card, TurnTakerController controller) : base(card, controller)
+        { }
+    }
+}
---
+++ Mod/Villains/Lung/Characters/LungTurnTakerController.cs
@@ -0,0 +1,17 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections;
+
+namespace Jp.ParahumansOfTheWormverse.Lung
+{
+    class LungTurnTakerController : TurnTakerController
+    {
+        public LungTurnTakerController(TurnTaker turnTaker, GameController gameController) : base(turnTaker, gameController)
+        { }
+
+        public override IEnumerator StartGame()
+        {
+            return base.StartGame();
+        }
+    }
+}
