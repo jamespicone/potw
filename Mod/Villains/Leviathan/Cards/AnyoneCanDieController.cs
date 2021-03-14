42827ad199b0fa544a12bf58893118c03f4d12d1
Softman25 <47977263+Softman25@users.noreply.github.com>
2021-03-14 18:03:46 +1030

    Some Leviathan cards

---
+++ Mod/Villains/Leviathan/Cards/AnyoneCanDieController.cs
@@ -0,0 +1,30 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jp.ParahumansOfTheWormverse.Leviathan
+{
+    public class AnyoneCanDieController : CardController
+    {
+        public AnyoneCanDieController(Card card, TurnTakerController controller) : base(card, controller)
+        { }
+        
+        public override System.Collections.IEnumerator Play()
+        {
+            // Leviathan deals the hero target with the highest HP 5 irreducible melee damage
+            var e = DealDamageToHighestHP(TurnTaker.CharacterCard, 1, c => c.IsHero && c.IsTarget && c.IsInPlay, c => 5, DamageType.Melee, true);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(e);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(e);
+            }
+        }
+
+    }
+}
---
+++ Mod/Villains/Leviathan/Cards/WaterShadowCardController.cs
@@ -0,0 +1,44 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections;
+
+namespace Jp.ParahumansOfTheWormverse.Leviathan
+{
+
+    // Whenever Leviathan would deal melee damage to a target, this card deals that target 2 irreducible melee damage. This card is indestructible
+
+    public class WaterShadowCardController : CardController
+    {
+        public WaterShadowCardController(Card card, TurnTakerController controller) : base(card, controller)
+        {
+            //This card is indestructible
+            base.AddThisCardControllerToList(CardControllerListType.MakesIndestructible);
+        }
+
+        public override void AddTriggers()
+        {
+          //Whenever Leviathan would deal melee damage to a target...  
+            AddTrigger<DealDamageAction>(
+                dda => dda.DamageType == DamageType.Melee && dda.DamageSource.Card == CharacterCard,
+                dda => RespondToMeleeDamage(dda),
+                TriggerType.DealDamage,
+                TriggerTiming.Before,
+                ActionDescription.DamageTaken
+            );
+        }
+
+        public IEnumerator RespondToMeleeDamage(DealDamageAction dda)
+        {
+            //...this card deals that target 2 irreducible melee damage
+            var e = DealDamage(this.Card, dda.Target, 2, DamageType.Melee, true);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(e);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(e);
+            }
+        }
+    }
+}
