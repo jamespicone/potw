99cdcdba9f245764f2953c1d502acd06a73e7511
kree1 <myopicmastermind@gmail.com>
2021-04-08 15:55:09 -0400

    Brockton Bay implemented

---
+++ Mod/Environments/BrocktonBay/PRTSquadCardController.cs
@@ -0,0 +1,27 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jp.ParahumansOfTheWormverse.BrocktonBay
+{
+    public class PRTSquadCardController : CardController
+    {
+        public PRTSquadCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            SpecialStringMaker.ShowNonEnvironmentTargetWithLowestHP(1, 1);
+        }
+
+        public override void AddTriggers()
+        {
+            // "At the end of the environment turn, this card deals the non-environment target with the lowest HP 2 projectile damage."
+            AddDealDamageAtEndOfTurnTrigger(base.TurnTaker, base.Card, (Card c) => !c.IsEnvironmentTarget, TargetType.LowestHP, 2, DamageType.Projectile, highestLowestRanking: 1, numberOfTargets: 1);
+            base.AddTriggers();
+        }
+    }
+}
---
+++ Mod/Environments/BrocktonBay/SuburbCardController.cs
@@ -0,0 +1,40 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jp.ParahumansOfTheWormverse.BrocktonBay
+{
+    public class SuburbCardController : CardController
+    {
+        public SuburbCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+        }
+
+        public override IEnumerator Play()
+        {
+            // "When this card enters play, destroy all other Suburb cards."
+            IEnumerator destroyCoroutine = base.GameController.DestroyCards(DecisionMaker, new LinqCardCriteria((Card c) => c.DoKeywordsContain("suburb") && c != base.Card), cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(destroyCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(destroyCoroutine);
+            }
+            yield break;
+        }
+    }
+}
