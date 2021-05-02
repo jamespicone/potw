d4ca8535ef4bff3438df1e71f9e2a75d0a97f771
kree1 <myopicmastermind@gmail.com>
2021-05-02 15:30:28 -0400

    Coil's Base initial implementation

---
+++ Mod/Environments/CoilsBase/AblativeCladdingCardController.cs
@@ -0,0 +1,27 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jp.ParahumansOfTheWormverse.CoilsBase
+{
+    public class AblativeCladdingCardController : CoilsBaseSelfDestructCardController
+    {
+        public AblativeCladdingCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "Reduce all toxic and melee damage by 2."
+            AddReduceDamageTrigger((DealDamageAction dda) => dda.DamageType == DamageType.Melee || dda.DamageType == DamageType.Toxic, (DealDamageAction dda) => 2);
+        }
+    }
+}
