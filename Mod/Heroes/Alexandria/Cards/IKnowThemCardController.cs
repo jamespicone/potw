3aeff0c1ed69b26203c013022682dc48fdd67af7
James Picone <jamesmpicone+github@gmail.com>
2021-07-18 21:42:55 +1000

    Add Alexandria files

---
+++ Mod/Heroes/Alexandria/Cards/IKnowThemCardController.cs
@@ -0,0 +1,15 @@
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jp.ParahumansOfTheWormverse.Alexandria
+{
+    public class IKnowThemCardController : CardController
+    {
+        public IKnowThemCardController(Card card, TurnTakerController controller) : base(card, controller)
+        { }
+    }
+}
---
+++ Mod/Heroes/Alexandria/Character/AlexandriaCharacterCardController.cs
@@ -0,0 +1,27 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jp.ParahumansOfTheWormverse.Alexandria
+{
+    public class AlexandriaCharacterCardController : HeroCharacterCardController
+    {
+        public AlexandriaCharacterCardController(Card card, TurnTakerController controller) : base(card, controller)
+        {
+        }
+
+        public override IEnumerator UseIncapacitatedAbility(int index)
+        {
+            yield break;
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            yield break;
+        }
+    }
+}
