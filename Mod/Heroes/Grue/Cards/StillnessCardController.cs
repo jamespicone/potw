8d3d96da106fbdbafcfd02ce7c422676d41c47a7
James Picone <jamesmpicone+github@gmail.com>
2021-10-04 21:26:14 +1100

    Add decklist and stub files

---
+++ Mod/Heroes/Grue/Cards/StillnessCardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+using Jp.ParahumansOfTheWormverse.Utility;
+
+namespace Jp.ParahumansOfTheWormverse.Grue
+{
+    public class StillnessCardController : CardController
+    {
+        public StillnessCardController(Card card, TurnTakerController controller) : base(card, controller)
+        { }
+    }
+}
---
+++ Mod/Heroes/Grue/Characters/GrueCharacterCardController.cs
@@ -0,0 +1,36 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Jp.ParahumansOfTheWormverse.Utility;
+
+namespace Jp.ParahumansOfTheWormverse.Grue
+{
+    public class GrueCharacterCardController : HeroCharacterCardController
+    {
+        public GrueCharacterCardController(Card card, TurnTakerController controller) : base(card, controller)
+        {
+        }
+
+        public override IEnumerator UseIncapacitatedAbility(int index)
+        {
+            IEnumerator e;
+            switch(index)
+            {
+                case 0:
+                case 1:
+                case 2:
+                default: yield break;
+            }
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            yield break;
+        }
+    }
+}
