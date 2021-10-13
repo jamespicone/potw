49f148c1c3b7034a3f1d21e9d65931ea535f149d
James Picone <jamesmpicone+github@gmail.com>
2021-10-13 22:44:19 +1100

    Start writing some code; include System.Collections in usings

--- Mod/Heroes/Grue/Cards/BlindingCardController.cs
+++ Mod/Heroes/Grue/Cards/BlindingCardController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 
@@ -14,5 +15,31 @@ namespace Jp.ParahumansOfTheWormverse.Grue
     {
         public BlindingCardController(Card card, TurnTakerController controller) : base(card, controller)
         { }
+
+        public override IEnumerator Play()
+        {
+            // "{GrueCharacter} deals 2 melee damage to a non-hero target.",
+            var e = GameController.SelectTargetsAndDealDamage(
+                HeroTurnTakerController,
+                new DamageSource(GameController, CharacterCard),
+                2,
+                DamageType.Melee,
+                numberOfTargets: 1,
+                optional: false,
+                requiredTargets: 1,
+                cardSource: GetCardSource()
+            );
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(e);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(e);
+            }
+
+            // "If you deal damage to a target with a Darkness card next to it you may destroy an Ongoing card"
+            yield break;
+        }
     }
 }
