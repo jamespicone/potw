84b1ac7120f239dd4aa292a8f9f7a2d708f88a15
James Picone <jamesmpicone+github@gmail.com>
2021-10-13 23:11:40 +1100

    Cover the Getaway

--- Mod/Heroes/Grue/Cards/CoverTheGetawayCardController.cs
+++ Mod/Heroes/Grue/Cards/CoverTheGetawayCardController.cs
@@ -15,5 +15,43 @@ namespace Jp.ParahumansOfTheWormverse.Grue
     {
         public CoverTheGetawayCardController(Card card, TurnTakerController controller) : base(card, controller)
         { }
+
+        public override IEnumerator Play()
+        {
+            // "Put a Darkness card next to each hero character card",
+            var e = GameController.SelectCardsAndDoAction(
+                HeroTurnTakerController,
+                new LinqCardCriteria(c => c.Is().Hero().Character(), "hero character"),
+                SelectionType.MoveCardNextToCard,
+                c => this.PutDarknessIntoPlay(c),
+                allowAutoDecide: true,
+                cardSource: GetCardSource()                
+            );
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(e);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(e);
+            }
+
+            // "You may destroy an Environment card"
+            e = GameController.SelectAndDestroyCard(
+                HeroTurnTakerController,
+                new LinqCardCriteria(c => c.Is().Environment(), "environment"),
+                optional: true,
+                responsibleCard: Card,
+                cardSource: GetCardSource()
+            );
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(e);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(e);
+            }
+        }
     }
 }
