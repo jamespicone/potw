fe9ac238b90e42e9fec3e825b6b31a12b85639c5
James Picone <jamesmpicone+github@gmail.com>
2021-10-17 22:55:48 +1100

    Stubs

---
+++ Mod/Villains/TheSimurgh/Cards/ACapabilityRevealedCardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+using Jp.ParahumansOfTheWormverse.Utility;
+
+namespace Jp.ParahumansOfTheWormverse.TheSimurgh
+{
+    public class ACapabilityRevealedCardController : CardController
+    {
+        public ACapabilityRevealedCardController(Card card, TurnTakerController controller) : base(card, controller)
+        { }
+    }
+}
---
+++ Mod/Villains/TheSimurgh/Cards/Traps/AnUnpleasantDiscoveryCardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+using Jp.ParahumansOfTheWormverse.Utility;
+
+namespace Jp.ParahumansOfTheWormverse.TheSimurgh
+{
+    public class AnUnpleasantDiscoveryCardController : CardController
+    {
+        public AnUnpleasantDiscoveryCardController(Card card, TurnTakerController controller) : base(card, controller)
+        { }
+    }
+}
