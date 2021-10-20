471fd35038117bbcf514e21188b4cba2385d9786
James Picone <jamesmpicone+github@gmail.com>
2021-10-20 23:05:24 +1100

    Simurgh work

---
+++ Mod/Villains/TheSimurgh/Characters/TheSimurghTurnTakerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+using Jp.ParahumansOfTheWormverse.Utility;
+
+namespace Jp.ParahumansOfTheWormverse.TheSimurgh
+{
+    class TheSimurghTurnTakerController : TurnTakerController
+    {
+        public TheSimurghTurnTakerController(TurnTaker turnTaker, GameController gameController) : base(turnTaker, gameController)
+        {}
+
+        public override IEnumerator StartGame()
+        {
+            var traps = FindCardsWhere(c => c.Owner == TurnTaker && c.DoKeywordsContain("trap"));
+
+            var e = GameController.ShuffleCardsIntoLocation(null, traps, TurnTaker.Revealed);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(e);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(e);
+            }
+
+            for (int i = 0; i < GameController.Game.H; ++i)
+            {
+                e = GameController.MoveCard(
+                    this,
+                    TurnTaker.Revealed.TopCard,
+                    TurnTaker.PlayArea,
+                    playCardIfMovingToPlayArea: false,
+                    flipFaceDown: true
+                );
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(e);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(e);
+                }
+            }
+
+            e = GameController.BulkMoveCards(
+                this,
+                TurnTaker.Revealed.Cards,
+                TurnTaker.OutOfGame
+            );
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(e);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(e);
+            }
+        }
+    }
+}
