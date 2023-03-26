8a281f4284b7820d02e721f1f7a3a09ff426d538
James Picone <jamesmpicone+github@gmail.com>
2023-03-26 12:06:02 +1100

    Use the new mechanism for face-up cards on decks for FollowsAllTheThreads

--- Mod/Heroes/Tattletale/Cards/FollowsAllTheThreadsCardController.cs
+++ Mod/Heroes/Tattletale/Cards/FollowsAllTheThreadsCardController.cs
@@ -16,30 +16,31 @@ namespace Jp.ParahumansOfTheWormverse.Tattletale
         {
         }
 
-        public override void AddStartOfGameTriggers()
+        public IEnumerator FlipOverCards()
         {
-            BuildTopDeckSpecialStrings();
-        }
-
-        private void BuildTopDeckSpecialStrings()
-        {
-            // "Play with the top card of each deck face up."
-            // Going to approximate this as "you may look at the top card of any deck at any time" to minimize weird interactions with, say, Ambuscade's Traps.
-            //this needs to be all turntakers in all zones.
-            foreach (TurnTaker tt in GameController.AllTurnTakers)
+            var decks = FindLocationsWhere(l => l.IsDeck);
+            foreach (var deck in decks)
             {
-                foreach (Location deck in tt.Decks.Where(deck => deck.IsRealDeck))
+                if (deck.NumberOfCards > 0 && ! deck.TopCard.IsFaceUp)
                 {
-                    var ss = SpecialStringMaker.ShowSpecialString(() => $"The top card of {deck.GetFriendlyName()} is {deck.TopCard.Title}.", relatedCards: () => tt.CharacterCards.Where(c => c.IsInPlayAndHasGameText));
-                    ss.Condition = () => Card.IsInPlayAndHasGameText && deck.HasCards && GameController.IsLocationVisibleToSource(deck, GetCardSource());
+                    deck.TopCard.SetFaceUp(true);
+                    deck.TopCard.SetIsPositionKnown(true);
                 }
             }
+            yield break;
+        }
+
+        public override IEnumerator Play()
+        {
+            return FlipOverCards();
         }
 
         public override void AddTriggers()
         {
             // "At the start of your turn, {TattletaleCharacter} deals herself 1 psychic damage."
             base.AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction pca) => DealDamage(base.CharacterCard, base.CharacterCard, 1, DamageType.Psychic, cardSource: GetCardSource()), TriggerType.DealDamage);
+
+            AddTrigger<GameAction>(ga => ga.CardSource != GetCardSource(), (a) => FlipOverCards(), TriggerType.Hidden, TriggerTiming.After);
             base.AddTriggers();
         }
 
