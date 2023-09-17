6002773886e473ca48006ea5bcc67de5bda4dca9
James Picone <jamesmpicone+github@gmail.com>
2023-09-17 21:45:31 +1000

    Get set up for random tests

---
+++ Test/Environment/BrocktonBay/RandomBrocktonBayTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.Engine.Controller;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Handelabra.Sentinels.UnitTest;
+
+namespace Jp.ParahumansOfTheWormverse.UnitTest.BrocktonBay
+{
+    [TestFixture()]
+    public class RandomBrocktonBayTests : RandomParahumanTest
+    {
+        [Test]
+        [Property("TestType", "Random")]
+        public void TestEnvRandom()
+        {
+            var gc = SetupRandomParahumanTest(overrideEnvironment: "Jp.ParahumansOfTheWormverse.BrocktonBay");
+            RunGame(gc);
+        }
+
+    }
+}
---
+++ Test/RandomParahumanTest.cs
@@ -0,0 +1,128 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using System.Linq;
+using Handelabra.Sentinels.UnitTest;
+using Handelabra.Sentinels.Engine.Model;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Jp.ParahumansOfTheWormverse.UnitTest
+{
+
+    public class RandomParahumanTest : RandomGameTest
+    {
+        protected GameController SetupRandomParahumanTest(
+            string overrideEnvironment = null,
+            List<string> useHeroes = null,
+            Dictionary<string, string> overrideVariants = null,
+            string overrideVillain = null
+        )
+        {
+            return SetupRandomGameController(
+                reasonable: false,
+                availableHeroes: ParahumansHeroes.Concat(DeckDefinition.AvailableHeroes),
+                availableVillains: ParahumansVillains.Concat(DeckDefinition.AvailableVillains),
+                availableEnvironments: ParahumansEnvironments.Concat(DeckDefinition.AvailableEnvironments),
+                overrideEnvironment: overrideEnvironment,
+                useHeroes: useHeroes,
+                randomizeUseHeroes: true,
+                overrideVariants: overrideVariants,
+                overrideVillain: overrideVillain
+            );
+        }
+
+        protected GameController SetupRandomParahumanTestWithTribunal(
+            List<string> useHeroes = null,
+            Dictionary<string, string> overrideVariants = null,
+            string overrideVillain = null
+        )
+        {
+            return SetupRandomGameController(
+                reasonable: false,
+                availableHeroes: ParahumansHeroes.Concat(DeckDefinition.AvailableHeroes),
+                availableVillains: ParahumansVillains.Concat(DeckDefinition.AvailableVillains),
+                availableEnvironments: ParahumansEnvironments.Concat(DeckDefinition.AvailableEnvironments),
+                overrideEnvironment: "CelestialTribunal",
+                useHeroes: useHeroes,
+                randomizeUseHeroes: true,
+                overrideVariants: overrideVariants,
+                overrideVillain: overrideVillain
+            );
+        }
+
+        protected GameController SetupRandomParahumanTestWithGuise(
+            string overrideEnvironment = null,
+            List<string> useHeroes = null,
+            Dictionary<string, string> overrideVariants = null,
+            string overrideVillain = null
+        )
+        {
+            Assert.Less(useHeroes.Count, 5);
+
+            useHeroes.Append("Guise");
+            overrideVariants["Guise"] = "GuiseCharacter";
+
+            return SetupRandomGameController(
+                reasonable: false,
+                availableHeroes: ParahumansHeroes.Concat(DeckDefinition.AvailableHeroes),
+                availableVillains: ParahumansVillains.Concat(DeckDefinition.AvailableVillains),
+                availableEnvironments: ParahumansEnvironments.Concat(DeckDefinition.AvailableEnvironments),
+                overrideEnvironment: overrideEnvironment,
+                useHeroes: useHeroes,
+                randomizeUseHeroes: true,
+                overrideVariants: overrideVariants,
+                overrideVillain: overrideVillain
+            );
+        }
+
+        protected GameController SetupRandomParahumanTestWithCompletionistGuise(
+            string overrideEnvironment = null,
+            List<string> useHeroes = null,
+            Dictionary<string, string> overrideVariants = null,
+            string overrideVillain = null
+        )
+        {
+            Assert.Less(useHeroes.Count, 5);
+
+            useHeroes.Append("Guise");
+            overrideVariants["Guise"] = "CompletionistGuiseCharacter";
+
+            return SetupRandomGameController(
+                reasonable: false,
+                availableHeroes: ParahumansHeroes.Concat(DeckDefinition.AvailableHeroes),
+                availableVillains: ParahumansVillains.Concat(DeckDefinition.AvailableVillains),
+                availableEnvironments: ParahumansEnvironments.Concat(DeckDefinition.AvailableEnvironments),
+                overrideEnvironment: overrideEnvironment,
+                useHeroes: useHeroes,
+                randomizeUseHeroes: true,
+                overrideVariants: overrideVariants,
+                overrideVillain: overrideVillain
+            );
+        }
+
+        protected GameController SetupRandomParahumanTestWithPWTempest(
+            string overrideEnvironment = null,
+            List<string> useHeroes = null,
+            Dictionary<string, string> overrideVariants = null,
+            string overrideVillain = null
+        )
+        {
+            Assert.Less(useHeroes.Count, 5);
+
+            useHeroes.Append("Tempest");
+            overrideVariants["Tempest"] = "PrimeWardensTempestCharacter";
+
+            return SetupRandomGameController(
+                reasonable: false,
+                availableHeroes: ParahumansHeroes.Concat(DeckDefinition.AvailableHeroes),
+                availableVillains: ParahumansVillains.Concat(DeckDefinition.AvailableVillains),
+                availableEnvironments: ParahumansEnvironments.Concat(DeckDefinition.AvailableEnvironments),
+                overrideEnvironment: overrideEnvironment,
+                useHeroes: useHeroes,
+                randomizeUseHeroes: true,
+                overrideVariants: overrideVariants,
+                overrideVillain: overrideVillain
+            );
+        }
+    }
+}
