82006801d3fc9ecde42e3ce61a637545c6bffada
James Picone <jamesmpicone+github@gmail.com>
2023-09-20 19:29:37 +1000

    Setup parametised tests for the random stuff

---
+++ Test/Environment/RandomEnvironmentTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.Engine.Controller;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Handelabra.Sentinels.UnitTest;
+
+namespace Jp.ParahumansOfTheWormverse.UnitTest.Environment
+{
+    [TestFixture(Category = "RandomCategory")]
+    [TestFixtureSource(nameof(RandomEnvironments))]
+    public class RandomEnvironmentTests : RandomParahumanTest
+    {
+        static TestFixtureData TestData(string env)
+        {
+            var identifier = "Jp.ParahumansOfTheWormverse." + env;
+            var ret = new TestFixtureData(identifier);
+            var definition = DeckDefinitionCache.GetDeckDefinition(identifier);
+
+            ret.TestName = definition.Name + " Random Tests";
+            return ret;
+        }
+
+        private static IEnumerable RandomEnvironments()
+        {
+            yield return TestData("BrocktonBay");
+            yield return TestData("CoilsBase");
+            yield return TestData("Kyushu");
+            yield return TestData("NewDelhi");
+        }
+
+        public RandomEnvironmentTests(string env)
+        {
+            EnvToUse = env;
+        }
+
+        private string EnvToUse = "";
+
+        [Test]
+        [Property("TestType", "Random")]
+        public void TestEnvRandom()
+        {
+            var gc = SetupRandomParahumanTest(overrideEnvironment: EnvToUse);
+            RunGame(gc);
+        }
+
+        [Test]
+        [Property("TestType", "Random")]
+        public void TestEnvRandomWithLabyrinth()
+        {
+            var gc = SetupRandomParahumanTestWithLabyrinth(overrideEnvironment: EnvToUse);
+            RunGame(gc);
+        }
+
+        [Test]
+        [Property("TestType", "Random")]
+        public void TestEnvRandomWithPWT()
+        {
+            var gc = SetupRandomParahumanTestWithPWTempest(overrideEnvironment: EnvToUse);
+            RunGame(gc);
+        }
+    }
+}
---
+++ Test/Villains/RandomVillainTests.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using System;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.Engine.Controller;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Handelabra.Sentinels.UnitTest;
+
+namespace Jp.ParahumansOfTheWormverse.UnitTest.Villain
+{
+    [TestFixture(Category = "RandomCategory")]
+    [TestFixtureSource(nameof(RandomVillains))]
+    public class RandomVillainTests : RandomParahumanTest
+    {
+        static TestFixtureData TestData(string villain, string variant, string name)
+        {
+            var ret = new TestFixtureData("Jp.ParahumansOfTheWormverse." + villain, variant);
+            ret.TestName = name + " Random Tests";
+            return ret;
+        }
+
+        private static IEnumerable RandomVillains()
+        {
+            yield return TestData("Behemoth", "BehemothCharacter", "Behemoth");
+            yield return TestData("Coil", "CoilCharacter", "Coil");
+            yield return TestData("Echidna", "EchidnaCharacter", "Echidna");
+            yield return TestData("Leviathan", "LeviathanCharacter", "Leviathan");
+            yield return TestData("Lung", "LungCharacter", "Lung");
+            yield return TestData("Slaughterhouse9", "Slaughterhouse9Character", "Slaughterhouse 9");
+            yield return TestData("TheMerchants", "SkidmarkCharacter", "Merchants");
+            yield return TestData("TheSimurgh", "TheSimurghCharacter", "Simurgh");
+        }
+
+
+
+        public RandomVillainTests(string villain, string variant)
+        {
+            VillainToUse = villain;
+            VariantToUse = variant;
+        }
+
+        private string VillainToUse = "";
+        private string VariantToUse = "";
+
+        [Test]
+        [Property("TestType", "Random")]
+        public void TestRandom()
+        {
+            var gc = SetupRandomParahumanTest(
+                overrideVillain: VillainToUse,
+                overrideVariants: new Dictionary<string, string> { { VillainToUse, VariantToUse } }
+            );
+            RunGame(gc);
+        }
+
+        [Test]
+        [Property("TestType", "Random")]
+        public void TestRandomWithLabyrinth()
+        {
+            var gc = SetupRandomParahumanTestWithLabyrinth(
+                overrideVillain: VillainToUse,
+                overrideVariants: new Dictionary<string, string> { { VillainToUse, VariantToUse } }
+            );
+            RunGame(gc);
+        }
+
+        [Test]
+        [Property("TestType", "Random")]
+        public void TestRandomWithPWTempest()
+        {
+            var gc = SetupRandomParahumanTestWithPWTempest(
+                overrideVillain: VillainToUse,
+                overrideVariants: new Dictionary<string, string> { { VillainToUse, VariantToUse } }
+            );
+            RunGame(gc);
+        }
+
+        [Test]
+        [Property("TestType", "Random")]
+        public void TestRandomWithTribunal()
+        {
+            var gc = SetupRandomParahumanTestWithTribunal(
+                overrideVillain: VillainToUse,
+                overrideVariants: new Dictionary<string, string> { { VillainToUse, VariantToUse } }
+            );
+            RunGame(gc);
+        }
+    }
+}
