0b90676d10003e58ef6cea67662b5260894b8857
James Picone <jamesmpicone+github@gmail.com>
2023-09-27 22:42:56 +1000

    Some tests for Martial Arts

--- Test/Heroes/Alexandria/MartialArtsTests.cs
+++ Test/Heroes/Alexandria/MartialArtsTests.cs
@@ -13,9 +13,117 @@ namespace Jp.ParahumansOfTheWormverse.UnitTest.Alexandria
     public class MartialArtsTests : ParahumanTest
     {
         [Test()]
-        public void TestModWorks()
+        public void TestCantDestroyHighHP()
         {
             SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Alexandria", "InsulaPrimalis");
+
+            StartGame();
+
+            var platform = GetMobileDefensePlatform().Card;
+            PlayCard("MartialArts");
+
+            AssertIsInPlay(platform);
+        }
+
+        [Test()]
+        public void TestCantDestroyCharacters()
+        {
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Alexandria", "InsulaPrimalis");
+
+            StartGame();
+
+            RemoveMobileDefensePlatform();
+
+            SetHitPoints(baron.CharacterCard, 3);
+            PlayCard("MartialArts");
+
+            AssertIsInPlay(baron.CharacterCard);
+        }
+
+        [Test()]
+        public void TestCanDestroyLowHP()
+        {
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Alexandria", "InsulaPrimalis");
+
+            StartGame();
+
+            var platform = GetMobileDefensePlatform().Card;
+            SetHitPoints(platform, 3);
+            PlayCard("MartialArts");
+
+            AssertInTrash(platform);
+        }
+
+        [Test()]
+        public void TestDestroyEffectsCauseStatusEffect()
+        {
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Alexandria", "InsulaPrimalis");
+
+            StartGame();
+
+            var platform = GetMobileDefensePlatform().Card;
+            SetHitPoints(platform, 3);
+
+            DecisionSelectCards = new Card[] { platform, baron.CharacterCard };
+            PlayCard("MartialArts");
+
+            AssertNumberOfStatusEffectsInPlay(1);
+
+            QuickHPStorage(alexandria);
+            DealDamage(baron.CharacterCard, alexandria.CharacterCard, 3, DamageType.Melee);
+            QuickHPCheck(0);
+        }
+
+        [Test()]
+        public void TestDestroyByDamageCausesStatusEffect()
+        {
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Alexandria", "InsulaPrimalis");
+
+            StartGame();
+
+            var platform = GetMobileDefensePlatform().Card;
+            PlayCard("MartialArts");
+
+            DecisionSelectCards = new Card[] { baron.CharacterCard };
+
+            AssertNumberOfStatusEffectsInPlay(0);
+
+            DealDamage(alexandria.CharacterCard, platform, 100, DamageType.Infernal);
+
+            AssertNumberOfStatusEffectsInPlay(1);
+
+            QuickHPStorage(alexandria);
+            DealDamage(baron.CharacterCard, alexandria.CharacterCard, 3, DamageType.Melee);
+            QuickHPCheck(0);
+        }
+
+        [Test()]
+        public void TestDestroyByDeathcaller()
+        {
+            SetupGameController("KaargraWarfang", "Jp.ParahumansOfTheWormverse.Alexandria", "InsulaPrimalis");
+
+            MoveAllCards(warfang, warfang.TurnTaker.FindSubDeck("TitleDeck"), warfang.TurnTaker.OutOfGame);
+            StartGame();
+            RemoveVillainCards();
+
+            var title = PlayCard("TitleDeathCaller");
+            var target = PlayCard("OrrimHiveminded");
+            
+            SetHitPoints(target, 3);           
+            PlayCard("MartialArts");
+
+            AssertAtLocation(title, alexandria.CharacterCard.BelowLocation);
+            AssertInTrash(target);
+
+            var target2 = PlayCard("OrrimHiveminded");
+            var target3 = PlayCard("SoulslayerPerith");
+            SetHitPoints(target2, 10);
+
+            DecisionAutoDecideIfAble = true;
+            DecisionSelectCards = new Card[] { target3 };
+            DealDamage(alexandria, target2, 9, DamageType.Infernal);
+            AssertNumberOfStatusEffectsInPlay(1);
+            AssertInTrash(target2);
         }
     }
 }
