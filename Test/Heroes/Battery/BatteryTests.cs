a5a9d3a7a7d17e6fff2afa3b174218e456059b56
James Picone <jamesmpicone+github@gmail.com>
2023-10-25 22:48:54 +1100

    Tests of Battery's main powers

--- Test/Heroes/Battery/BatteryTests.cs
+++ Test/Heroes/Battery/BatteryTests.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using Handelabra.Sentinels.UnitTest;
+using Jp.ParahumansOfTheWormverse.Battery;
 
 namespace Jp.ParahumansOfTheWormverse.UnitTest.Battery
 {
@@ -13,9 +14,46 @@ namespace Jp.ParahumansOfTheWormverse.UnitTest.Battery
     public class BatteryTests : ParahumanTest
     {
         [Test()]
-        public void TestModWorks()
+        public void TestUnchargedPower()
         {
             SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Battery", "InsulaPrimalis");
+
+            StartGame();
+
+            AssertNumberOfUsablePowers(battery.CharacterCard, 1);
+
+            Assert.IsFalse(battery.CharacterCardController.IsCharged(battery.CharacterCard));
+            QuickHandStorage(battery);
+            UsePower(battery.CharacterCard);
+            QuickHandCheck(1);
+            Assert.IsTrue(battery.CharacterCardController.IsCharged(battery.CharacterCard));
+
+            AssertNumberOfUsablePowers(battery.CharacterCard, 1);
+        }
+
+        [Test()]
+        public void TestChargedPower()
+        {
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Battery", "InsulaPrimalis");
+
+            StartGame();
+
+            AssertNumberOfUsablePowers(battery.CharacterCard, 1);
+
+            Assert.IsFalse(battery.CharacterCardController.IsCharged(battery.CharacterCard));
+            UsePower(battery.CharacterCard);
+            Assert.IsTrue(battery.CharacterCardController.IsCharged(battery.CharacterCard));
+
+            AssertNumberOfUsablePowers(battery.CharacterCard, 1);
+
+            var magnetism = PutInHand("Magnetism");
+            DecisionSelectCard = magnetism;
+            AssertInHand(magnetism);
+            UsePower(battery.CharacterCard);
+            AssertIsInPlay(magnetism);
+
+            Assert.IsFalse(battery.CharacterCardController.IsCharged(battery.CharacterCard));
+            AssertNumberOfUsablePowers(battery.CharacterCard, 0);
         }
     }
 }
