e532c08303a47433e5097937495b44710c39d89c
James Picone <jamesmpicone+github@gmail.com>
2023-12-04 21:16:52 +1100

    Test no time to rest

--- Test/Heroes/Battery/NoTimeToRestTests.cs
+++ Test/Heroes/Battery/NoTimeToRestTests.cs
@@ -13,9 +13,52 @@ namespace Jp.ParahumansOfTheWormverse.UnitTest.Battery
     public class NoTimeToRestTests : ParahumanTest
     {
         [Test()]
-        public void TestModWorks()
+        public void TestDestroyedWithMagnetism()
         {
             SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Battery", "InsulaPrimalis");
+
+            StartGame();
+
+            var magnetism = PlayCard("Magnetism");
+            UsePower(battery);
+
+            var platform = GetMobileDefensePlatform().Card;
+
+            PlayCard("NoTimeToRest");
+
+            var charge = GetCard("StaticCharge");
+            MoveCard(battery, charge, battery.HeroTurnTaker.Hand);
+
+            DecisionSelectCards = new Card[] { charge };
+
+            QuickHandStorage(battery);
+            UsePower(magnetism);
+            QuickHandCheck(0); // drew a card, played a card
+
+            AssertIsInPlay(charge);
+        }
+
+        [Test()]
+        public void TestDestroyedWithDamage()
+        {
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Battery", "InsulaPrimalis");
+
+            StartGame();
+
+            var platform = GetMobileDefensePlatform().Card;
+
+            PlayCard("NoTimeToRest");
+
+            var charge = GetCard("StaticCharge");
+            MoveCard(battery, charge, battery.HeroTurnTaker.Hand);
+
+            DecisionSelectCards = new Card[] { charge };
+
+            QuickHandStorage(battery);
+            DealDamage(battery, platform, 30, DamageType.Infernal);
+            QuickHandCheck(0); // drew a card, played a card
+
+            AssertIsInPlay(charge);
         }
     }
 }
