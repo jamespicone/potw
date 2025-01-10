f84367124070267c6d122f8ec62aa88cb9400cd6
James Picone <jamesmpicone+github@gmail.com>
2025-01-10 20:43:45 +1100

    Add test for Dragon being the Representative of Earth.

--- Test/Heroes/Dragon/DragonTests.cs
+++ Test/Heroes/Dragon/DragonTests.cs
@@ -13,9 +13,66 @@ namespace Jp.ParahumansOfTheWormverse.UnitTest.Dragon
     public class DragonTests : ParahumanTest
     {
         [Test()]
-        public void TestModWorks()
+        public void TestPhases()
         {
             SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Dragon", "InsulaPrimalis");
+
+            StartGame();
+
+            RemoveVillainCards();
+            RemoveVillainTriggers();
+
+            var tokenPool = dragon.CharacterCard.FindTokenPool("FocusPool");
+
+            DecisionDoNotActivatableAbility = true;
+
+            GoToStartOfTurn(dragon);
+            AssertCurrentTurnPhase(dragon, Phase.Start);
+            AssertTokenPoolCount(tokenPool, 4);
+
+            EnterNextTurnPhase();
+            AssertCurrentTurnPhase(dragon, Phase.Unknown);
+            AssertTokenPoolCount(tokenPool, 4);
+
+            EnterNextTurnPhase();
+            AssertCurrentTurnPhase(dragon, Phase.End);
+            AssertTokenPoolCount(tokenPool, 0);
         }
+
+        [Test()]
+        public void TestRepOfEarth()
+        {
+            SetupGameController("BaronBlade", "Legacy", "TheCelestialTribunal");
+
+            StartGame();
+
+            RemoveVillainCards();
+            RemoveVillainTriggers();
+
+            SelectFromBoxForNextDecision("Jp.ParahumansOfTheWormverse.DragonCharacter", "Jp.ParahumansOfTheWormverse.Dragon");
+            var rep = PlayCard("RepresentativeOfEarth");
+            var dragonCard = rep.NextToLocation.Cards.FirstOrDefault();
+            Assert.That(dragonCard, Is.Not.Null);
+            Assert.That(dragonCard.Identifier, Is.EqualTo("DragonCharacter"));
+
+            var tokenPool = dragonCard.FindTokenPool("FocusPool");
+
+            StackDeck("CalledToJudgement");
+
+            GoToStartOfTurn(env);
+            AssertCurrentTurnPhase(env, Phase.Start);
+            AssertTokenPoolCount(tokenPool, 4);
+            EnterNextTurnPhase();
+
+            AssertCurrentTurnPhase(env, Phase.PlayCard);
+            AssertTokenPoolCount(tokenPool, 4);
+            EnterNextTurnPhase();
+
+            AssertCurrentTurnPhase(env, Phase.End);
+            AssertTokenPoolCount(tokenPool, 0);
+            EnterNextTurnPhase();
+        }
+
+        // Crashes if it's in the environment via representative of earth
     }
 }
