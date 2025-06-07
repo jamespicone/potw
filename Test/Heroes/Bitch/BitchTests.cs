c03895bc3fc17cc9d3ad8dd811b7cde2530fffe6
James Picone <jamesmpicone+github@gmail.com>
2025-06-07 15:58:58 +1000

    Add some bitch unit tests. AI generated!

--- Test/Heroes/Bitch/BitchTests.cs
+++ Test/Heroes/Bitch/BitchTests.cs
@@ -73,5 +73,85 @@ namespace Jp.ParahumansOfTheWormverse.UnitTest.Bitch
 
             QuickHPCheck(-2, -2);
         }
+
+        [Test()]
+        public void TestIncapFirstAbilityHealsTarget()
+        {
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Bitch", "Legacy", "InsulaPrimalis");
+            StartGame();
+
+            // Set legacy's HP lower so we can heal
+            SetHitPoints(legacy, 20);
+
+            // Get Bitch incapacitated
+            DealDamage(baron, bitch, 30, DamageType.Melee);
+            AssertIncapacitated(bitch);
+
+            // Track HP changes
+            QuickHPStorage(legacy);
+
+            // Use first incap ability, selecting Legacy to heal
+            DecisionSelectCard = legacy.CharacterCard;
+            UseIncapacitatedAbility(bitch, 0);
+
+            // Check that Legacy gained 2 HP
+            QuickHPCheck(2);
+        }
+
+        [Test()]
+        public void TestIncapSecondAbilityMovesCardFromTrashToHand()
+        {
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Bitch", "Legacy", "InsulaPrimalis");
+            StartGame();
+
+            // Get Bitch incapacitated
+            DealDamage(baron, bitch, 30, DamageType.Melee);
+            AssertIncapacitated(bitch);
+
+            // Put a card in Legacy's trash
+            var motivationalCharge = PutInTrash("MotivationalCharge");
+            AssertInTrash(motivationalCharge);
+
+            // Use second incap ability
+            DecisionSelectTurnTaker = legacy.TurnTaker;
+            DecisionSelectCard = motivationalCharge;
+            UseIncapacitatedAbility(bitch, 1);
+
+            // Verify card moved from trash to hand
+            AssertInHand(motivationalCharge);
+        }
+
+        [Test()]
+        public void TestIncapThirdAbilityReducesEnvironmentDamage()
+        {
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Bitch", "Legacy", "InsulaPrimalis");
+            StartGame();
+
+            RemoveVillainCards();
+            RemoveVillainTriggers();
+
+            // Get Bitch incapacitated
+            DealDamage(baron, bitch, 30, DamageType.Melee);
+            AssertIncapacitated(bitch);
+
+            // Get an environment card to deal damage
+            Card raptor = PlayCard("VelociraptorPack");
+            QuickHPStorage(legacy);
+
+            // Use third incap ability 
+            UseIncapacitatedAbility(bitch, 2);
+
+            // Deal damage with environment card - should be reduced by 2
+            DealDamage(raptor, legacy, 3, DamageType.Melee);
+            QuickHPCheck(-1); // 3 damage reduced by 2
+
+            // Go to start of Bitch's next turn
+            GoToStartOfTurn(bitch);
+
+            // Damage reduction should be gone now
+            QuickHPStorage(legacy);
+            DealDamage(raptor, legacy, 3, DamageType.Melee);
+            QuickHPCheck(-3); // Full damage
+        }
     }
 }
--- Test/Heroes/Bitch/VeterinaryCareTests.cs
+++ Test/Heroes/Bitch/VeterinaryCareTests.cs
@@ -13,9 +13,51 @@ namespace Jp.ParahumansOfTheWormverse.UnitTest.Bitch
     public class VeterinaryCareTests : ParahumanTest
     {
         [Test()]
-        public void TestModWorks()
+        public void TestVeterinaryCareHealsAllDogs()
         {
             SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Bitch", "InsulaPrimalis");
+
+            StartGame();
+
+            // Put some dogs into play
+            Card brutus = PlayCard("Brutus");
+            Card judas = PlayCard("Judas");
+            Card angelica = PlayCard("Angelica");
+
+            // Damage the dogs
+            SetHitPoints(brutus, 2);
+            SetHitPoints(judas, 3);
+            SetHitPoints(angelica, 1);
+
+            // Play Veterinary Care
+            DecisionDoNotSelectCard = SelectionType.PlayCard;
+            Card vetCare = PutInHand("VeterinaryCare");
+            PlayCard(vetCare);
+
+            // Verify all dogs are at full HP
+            AssertIsAtMaxHP(brutus);
+            AssertIsAtMaxHP(judas);
+            AssertIsAtMaxHP(angelica);
+
+            AssertIsInPlay(brutus, judas, angelica);
+        }
+
+        [Test()]
+        public void TestVeterinaryCareDrawsAndPlaysCard()
+        {
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Bitch", "InsulaPrimalis");
+            StartGame();
+
+            var brutus = PutInHand("Brutus");
+            Card vetCare = PutInHand("VeterinaryCare");
+
+            QuickHandStorage(bitch);
+
+            DecisionSelectCardToPlay = brutus;
+            PlayCard(vetCare);
+
+            QuickHandCheck(-1);
+            AssertIsInPlay(brutus);
         }
     }
 }
