591d866925d8dbaa85d81231d8de18d78652d1da
James Picone <jamesmpicone+github@gmail.com>
2025-06-08 17:27:22 +1000

    Tests for Kill.

--- Test/Heroes/Bitch/KillTests.cs
+++ Test/Heroes/Bitch/KillTests.cs
@@ -13,9 +13,114 @@ namespace Jp.ParahumansOfTheWormverse.UnitTest.Bitch
     public class KillTests : ParahumanTest
     {
         [Test()]
-        public void TestModWorks()
+        public void TestKillNoDogs()
         {
+            // Arrange
             SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Bitch", "InsulaPrimalis");
+            StartGame();
+
+            RemoveMobileDefensePlatform();
+
+            // Get player's character card and Baron Blade's character card
+            Card bitch = GetCard("BitchCharacter");
+            Card baron = GetCard("BaronBladeCharacter");
+
+            // Put Kill card in hand
+            Card kill = PutInHand("Kill");
+
+            // Select Baron Blade as target
+            DecisionSelectTarget = baron;
+
+            // Act - play Kill
+            PlayCard(kill);
+
+            // Assert - with no dogs in play, should deal 0 damage (3 * 0)
+            AssertIsAtMaxHP(baron);
+        }
+
+        [Test()]
+        public void TestKillWithOneDog()
+        {
+            // Arrange
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Bitch", "InsulaPrimalis");
+            StartGame();
+
+            RemoveMobileDefensePlatform();
+
+            Card brutus = PlayCard("Brutus");
+            Card baron = GetCard("BaronBladeCharacter");
+            Card kill = PutInHand("Kill");
+
+            DecisionSelectTarget = baron;
+            QuickHPStorage(baron);
+
+            AssertDamageSource(bitch.CharacterCard);
+            AssertDamageType(DamageType.Melee);
+
+            // Act
+            PlayCard(kill);
+
+            // Assert - with 1 dog in play, should deal 3 damage (3 * 1)
+            QuickHPCheck(-3);
+        }
+
+        [Test()]
+        public void TestKillWithThreeDogs()
+        {
+            // Arrange
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Bitch", "InsulaPrimalis");
+            StartGame();
+
+            RemoveMobileDefensePlatform();
+
+            Card brutus = PlayCard("Brutus");
+            Card judas = PlayCard("Judas");
+            Card angelica = PlayCard("Angelica");
+            Card baron = GetCard("BaronBladeCharacter");
+            Card kill = PutInHand("Kill");
+
+            DecisionSelectTarget = baron;
+            QuickHPStorage(baron);
+
+            AssertDamageSource(bitch.CharacterCard);
+            AssertDamageType(DamageType.Melee);
+
+            // Act
+            PlayCard(kill);
+
+            // Assert - with 3 dogs in play, should deal 9 damage (3 * 3) 
+            QuickHPCheck(-9);
+        }
+
+        [Test()]
+        public void TestKillWithDestroyedDogs()
+        {
+            // Arrange
+            SetupGameController("BaronBlade", "Jp.ParahumansOfTheWormverse.Bitch", "InsulaPrimalis");
+            StartGame();
+
+            RemoveMobileDefensePlatform();
+
+            Card brutus = PlayCard("Brutus");
+            Card judas = PlayCard("Judas");
+            Card angelica = PlayCard("Angelica");
+            Card baron = GetCard("BaronBladeCharacter");
+            Card kill = PutInHand("Kill");
+
+            // Destroy one dog
+            DestroyCard(brutus);
+
+            DecisionSelectTarget = baron;
+            QuickHPStorage(baron);
+
+            AssertDamageSource(bitch.CharacterCard);
+            AssertDamageType(DamageType.Melee);
+
+            // Act
+            PlayCard(kill);
+
+            // Assert - with 2 dogs in play (1 destroyed), should deal 6 damage (3 * 2)
+            QuickHPCheck(-6);
         }
     }
 }
